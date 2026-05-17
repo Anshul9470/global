9311d9eb6fdf36b5df1304f9f359444c4959da4a
anshul9470 <tushartv22235@gmail.com>
2026-05-17 23:51:29 +0530

    chore: ignore backend folder

--- backend/src/routes/callRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const callController = require('../controllers/callcontroller');
-
-router.post('/submit-call', callController.submitCall);
-
-module.exports = router;
--- backend/src/routes/formroutesl.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const landingFormController = require("../controllers/landingFormController");
-
-// Route to handle landing form submission
-router.post("/submit-landing", landingFormController.submitLandingForm);
-
-module.exports = router;
